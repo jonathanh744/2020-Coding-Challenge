ed8671faf4dc641c719b3795771571964d2d5dd5
joanthanh744 <jonathanh7441@gmail.com>
2024-05-09 19:05:28 -0400

    completed

--- scoreboardOG.js
+++ scoreboardOG.js
@@ -1,21 +1,9 @@
 var temp_scoreboard
-var order
 function display_scoreboard(scoreboard){
-
-  // scoreboard.sort(function(a, b) {
-  //   return b.score - a.score;
-  // });
-
-  order=[];
   $("#teams").empty();
-  $.each(scoreboard.toSorted(function(a, b) {
-    return b.score - a.score;
-  }), function(index, team){
-    order.push(team.id)
+  $.each(scoreboard, function(index, team){
     addTeamView(team.id, team.name, team.score);
-
   });
-  console.log(order)
 }
 
 function addTeamView(id, name, score){
@@ -37,30 +25,22 @@ function addTeamView(id, name, score){
   $("#teams").append(team_template);
 }
 
-
-
 function increase_score(id){
-  original_id= id
   var team_id = {"id": id}
-  // console.log ("INI")
-  // console.log(team_id)
-  // console.log(temp_scoreboard[id-1].name)
-  // console.log("button pressed "+order[id-1])
-  // console.log("team name  "+temp_scoreboard[original_id-1].name)
-  team_id = {"id": temp_scoreboard[order[id-1]-1].id}
-  // console.log("team id  "+String(temp_scoreboard[original_id-1].name))
   $.ajax({
     type: "POST",
     url: "increase_score",                
     dataType : "json",
     contentType: "application/json; charset=utf-8",
-    data : JSON.stringify({"id": original_id}),
+    data : JSON.stringify(team_id),
     success: function(result){
-      
-      temp_scoreboard[original_id-1].score+=1
+      console.log(team_id)
+      //console.log(temp_scoreboard[team_id.id-1].name)
+      console.log(temp_scoreboard[id-1].score)
+      temp_scoreboard[id-1].score+=1
 
       display_scoreboard(temp_scoreboard)
-      // console.log("SUCCESS");
+
     },
     error: function(request, status, error){
         console.log("Error");
@@ -69,7 +49,23 @@ function increase_score(id){
         console.log(error)
     }
   });
- 
+  $.ajax({
+    type: "GET",
+    url: "static/scoreboard.js",
+    success: function(result){
+        
+    },
+    error: function(request, status, error){
+        console.log("Error");
+        console.log(request)
+        console.log(status)
+        console.log(error)
+    }
+  });
+
+
+
+
 }
 
 
--- serverOG.py
+++ serverOG.py
@@ -44,19 +44,13 @@ def show_scoreboard():
 @app.route('/increase_score', methods=['GET', 'POST'])
 def increase_score():
     global scoreboard
-    scores = [team["score"] for team in scoreboard]
-    print(scores)
-    
 
     json_data = request.get_json()   
     team_id = json_data["id"]  
-    print(scores[team_id-1])
-    temp=scores[team_id-1]
     
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
-     
 
             
 
